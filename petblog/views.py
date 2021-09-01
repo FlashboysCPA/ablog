ee50d6829e06cc40148f67bffa660c6ed60657be
jason <jason93@vt.edu>
2021-09-01 13:35:57 -0400

    update 9.1.21

--- petblog/views.py
+++ petblog/views.py
@@ -4,3 +4,4 @@ def home(request):
     return render(request, 'home.html', {})
 # Create your views here.
 # push video up to heroku for web hosting https://www.youtube.com/watch?v=UkokhawLKDU&list=PLCC34OHNcOtrZnQI6ZLvGPUWfQ6oh-D6H&index=13
+# test git, turn on virt envirnment and then python manage.py runserver
