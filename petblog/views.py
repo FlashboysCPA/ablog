c028cff656d6b576da4b4960df6211df3d60c26f
jason <jason93@vt.edu>
2021-09-01 15:00:55 -0400

    update 9.1.21

--- petblog/views.py
+++ petblog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
 
-def home(request):
-    return render(request, 'home.html', {})
 # Create your views here.
 # push video up to heroku for web hosting https://www.youtube.com/watch?v=UkokhawLKDU&list=PLCC34OHNcOtrZnQI6ZLvGPUWfQ6oh-D6H&index=13
 # test git, turn on virt envirnment and then python manage.py runserver
+
+#from .forms import PostForm
+#from django.urls import reverse_lazy
+
+class HomeView(ListView):
+	model = Post
+	template_name = 'home.html'
+	#ordering = ['-pub_date']
+	#success_message = 'List successfully saved!!!!'
